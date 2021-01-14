577c1aa73b5a37924021ba2c3136972d2f9e971b
Antal Asztrik <antal.asztrik@gmail.com>
2021-01-14 23:22:41 +0100

    feat: add text, basic styling to home component

--- src/Home.svelte
+++ src/Home.svelte
@@ -3,9 +3,28 @@
 </script>
 
 <section>
-  <h1>Hello I'm FEAR!</h1>
+  <h1>Hello, itt FEAR!</h1>
+  <p>A FEAR <em>(Fejlett Adminisztrációs Egyetemi Rendszer)</em> egy tetszőleges egyetem adminisztrálására lesz fejlesztve, melynek célja, hogy kényelmes, biztonságos és gyors adminisztrációt tegyen lehetővé minden felhasználó számára.
+  </p>
+  <p><a href="/">Próbálja ki ingyenesen!</a></p>
 </section>
 
-<style>
+<style lang="scss">
+  section {
+    max-width: 1200px;
+    margin: auto;
 
+    h1 {
+      font-size: 3.9em;
+    }
+    p {
+      font-size: 2.7em;
+      font-weight: 600;
+
+      a {
+        padding: 3px 10px;
+        border-radius: 15px;
+      }
+    }
+  }
 </style>
