43035748e5efb9b20aaa43f462916264157e26f3
Antal Asztrik <antal.asztrik@gmail.com>
2021-01-17 00:49:16 +0100

    feat: add nav list items while logged in

--- src/LoggedInNav.svelte
+++ src/LoggedInNav.svelte
@@ -3,8 +3,12 @@
 </script>
 
 <ul>
+  <li><a href="/"><span>Saját adatok</span></a></li>
+  <li><a href="/"><span>Tanulmányok</span></a></li>
   <li><a href="/"><span>Tárgyak</span></a></li>
   <li><a href="/"><span>Vizsgák</span></a></li>
+  <li><a href="/"><span>Pénzügyek</span></a></li>
+  <li><a href="/"><span>Információ</span></a></li>
 </ul>
 
 <style lang="scss">
