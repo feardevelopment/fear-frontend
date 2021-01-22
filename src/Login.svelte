aaa367602690207ddd4027f38c5d1ef463dfaeb0
unknown <antal.asztrik@gmail.com>
2021-01-22 23:23:37 +0100

    feat: add input fields to login component

--- src/Login.svelte
+++ src/Login.svelte
@@ -3,7 +3,14 @@
 </script>
 
 <div>
-    <p>Hi I'm the Login component</p>
+    <h1>Bejelentkezés</h1>
+    <form>
+        <label for="username">Felhasználónév:</label>
+        <input type="text" id="username">
+        <label for="password">Jelszó:</label>
+        <input type="password" id="password">
+        <input type="button" value="Bejelentkezés">
+    </form>
 </div>
 
 <style lang="scss">
