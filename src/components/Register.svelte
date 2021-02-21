820aaf079b85ad38632c2fd6f4358e9c008e820e
Antal Asztrik <antal.asztrik@gmail.com>
2021-02-21 15:00:31 +0100

    feat: create register component

---
+++ src/components/Register.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+</script>
+
+<div>
+    <h1>Regisztráció</h1>
+    <form>
+        <label for="username">Felhasználónév:</label>
+        <input type="text" id="username">
+        <label for="password">Jelszó:</label>
+        <input type="password" id="password">
+        <label for="password">Jelszó ismét:</label>
+        <input type="password" id="password">
+        <input type="button" value="Regisztráció">
+    </form>
+</div>
+
+<style lang="scss">
+
+</style>
