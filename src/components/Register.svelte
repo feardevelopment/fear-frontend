5e28f46bd5066d0ca1bd6ee77cedc4efd8b2e01f
Antal Asztrik <antal.asztrik@gmail.com>
2021-02-21 17:41:30 +0100

    feat: add http request to component

--- src/components/Register.svelte
+++ src/components/Register.svelte
@@ -1,17 +1,36 @@
 <script lang="ts">
+  let email = '';
+  let password = '';
 
+  async function register() {
+    const res = await fetch('http://localhost:3000/register', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const text = await res.text();
+    
+    if (res.ok) {
+      return text;
+    } else {
+      throw new Error(text);
+    }
+  }
 </script>
 
 <div>
     <h1>Regisztráció</h1>
     <form>
-        <label for="username">Felhasználónév:</label>
-        <input type="text" id="username">
+        <label for="email">Email:</label>
+        <input bind:value={email} type="text" id="email">
         <label for="password">Jelszó:</label>
-        <input type="password" id="password">
-        <label for="password">Jelszó ismét:</label>
-        <input type="password" id="password">
-        <input type="button" value="Regisztráció">
+        <input bind:value={password} type="password" id="password">
+        <label for="password-again">Jelszó ismét:</label>
+        <input type="password" id="password-again">
+        <input on:click={register} type="button" value="Regisztráció">
     </form>
 </div>
 
