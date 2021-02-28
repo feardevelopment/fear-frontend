296f7f10c76ec0288c87a350220cf96ea031d58e
Antal Asztrik <antal.asztrik@gmail.com>
2021-02-28 14:27:55 +0100

    style: add some styling to the register component

--- src/components/Register.svelte
+++ src/components/Register.svelte
@@ -16,17 +16,49 @@
   }
 </script>
 
-<div>
+<section>
     <h1>Regisztráció</h1>
     <form>
-        <label for="email">Email:</label>
-        <input bind:value={email} type="text" id="email">
-        <label for="password">Jelszó:</label>
-        <input bind:value={password} type="password" id="password">
-        <input on:click={register} type="button" value="Regisztráció">
+      <label for="email">Email:</label>
+      <input bind:value={email} type="text" id="email">
+      <label for="password">Jelszó:</label>
+      <input bind:value={password} type="password" id="password">
+      <input on:click={register} type="button" value="Regisztráció">
     </form>
-</div>
+</section>
 
 <style lang="scss">
+  section {
+    width: min-content;
+    height: min-content;
+    max-width: 960px;
+    padding: 20px 50px;
+    margin: 0 auto;
 
+    h1 {
+      font-size: 32px;
+      margin-bottom: 35px;
+      letter-spacing: 2px;
+    }
+
+    form {
+      label {
+        display: block;
+        font-size: 18px;
+        letter-spacing: 1.2px;
+        font-weight: 500;
+        opacity: 80%;
+      }
+
+      input {
+        font-size: 20px;
+        margin: 8px 0 15px;
+        padding: 3px 8px;
+
+        &[type="button"] {
+          cursor: pointer;
+        }
+      }
+    }
+  }
 </style>
