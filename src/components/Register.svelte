72e867ce9a4d2c6ee23a537b95de561db66d8f1b
Antal Asztrik <antal.asztrik@gmail.com>
2021-02-28 16:49:36 +0100

    style: add style changes to component validation

--- src/components/Register.svelte
+++ src/components/Register.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-  let data = { email: '', password: '' };
+  let formData = { email: '', password: '' };
   let errors = { email: '', password: '' };
   let message: string;
 
   const submitHandler = () => {
     let valid = true;
     
-    if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(data.email)) {
+    if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(formData.email)) {
       valid = false;
       errors.email = 'Helytelen email!';
     } else {
       errors.email = '';
     }
 
-    if (data.password.length < 8 || data.password.length > 24) {
+    if (formData.password.length < 8 || formData.password.length > 24) {
       valid = false;
       errors.password = 'A jelszónak 8 és 24 karakter között kell lennie!';
     } else {
@@ -28,13 +28,14 @@
   async function register() {
     const res = await fetch('http://localhost:3000/register', {
       method: 'POST',
-      body: JSON.stringify(data),
+      body: JSON.stringify(formData),
       headers: {
         'Content-Type': 'application/json'
       }
     });
 
     message = (await res.json()).message;
+    console.log(message);
   }
 </script>
 
@@ -42,10 +43,10 @@
     <h1>Regisztráció</h1>
     <form on:submit|preventDefault={submitHandler}>
       <label for="email">Email:</label>
-      <input bind:value={data.email} type="text" id="email">
+      <input bind:value={formData.email} type="text" id="email" class:error-input="{errors.email}" autocomplete="false">
       <p class="error">{ errors.email }</p>
       <label for="password">Jelszó:</label>
-      <input bind:value={data.password} type="password" id="password">
+      <input bind:value={formData.password} type="password" id="password" class:error-input="{errors.password}">
       <p class="error">{ errors.password }</p>
       <button type="submit">Regisztráció</button>
     </form>
@@ -58,11 +59,13 @@
     max-width: 960px;
     padding: 20px 50px;
     margin: 0 auto;
+    background-color: #EDEDED;
 
     h1 {
       font-size: 32px;
       margin-bottom: 35px;
       letter-spacing: 2px;
+      font-weight: bold;
     }
 
     form {
@@ -78,6 +81,8 @@
       input {
         font-size: 20px;
         padding: 3px 8px;
+        border: 1.5px solid silver;
+        border-radius: 5px;
       }
 
       button {
@@ -87,6 +92,10 @@
         cursor: pointer;
       }
     }
+  }
+
+  .error-input {
+    border: 2px solid red;
   }
 
   p.error {
