232d76888ab19102cd9ab36765c9fd7323bffaa6
Antal Asztrik <antal.asztrik@gmail.com>
2021-03-02 13:52:14 +0100

    feat: add header and footer to the login page route

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,5 +1,9 @@
 <script>
+  import Header from '$components/Header.svelte';
   import Login from '$components/Login.svelte';
+  import Footer from '$components/Footer.svelte';
 </script>
 
+<Header />
 <Login />
+<Footer />
