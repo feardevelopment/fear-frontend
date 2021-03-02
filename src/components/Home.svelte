87807da8552d1f210d0008a73aa317efd60874bd
Antal Asztrik <antal.asztrik@gmail.com>
2021-03-02 17:26:55 +0100

    fix: fix one line after merging

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -9,7 +9,7 @@
       <p class="desc">A FEAR célja, hogy kényelmes, biztonságos és gyors adminisztrációt tegyen lehetővé minden felhasználó számára.</p>
       <img src="5251.png" alt="University Graphics">
       <a class="login-cta" href="/login"><p>Jelentkezzen be!</p></a>
-      <a class="register-cta" href="/"><p>Még nincs fiókja? Regisztráljon itt!</p></a>
+      <p class="register-text">Még nincs fiókja? Regisztráljon <a href="/register">itt!</a></p>
     </article>
   </section>
 </main>
