6f826f4bd8182dcebcd5e35e60269d750aafcc6c
Antal Asztrik <antal.asztrik@gmail.com>
2021-03-04 21:06:39 +0100

    feat: add layout component for better route handling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,5 @@
 <script>
 	import Landing from '$components/Landing.svelte';
-	import Footer from '$components/Footer.svelte';
-	import Header from '$components/Header.svelte';
 </script>
 
-<Header />
 <Landing />
-<Footer />
--- src/routes/register/index.svelte
+++ src/routes/register/index.svelte
@@ -1,9 +1,5 @@
 <script>
-  import Header from '$components/Header.svelte';
   import Register from '$components/Register.svelte';
-  import Footer from '$components/Footer.svelte';
 </script>
 
-<Header />
 <Register />
-<Footer />
